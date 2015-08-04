5ffd12cd29d8f7c11653ef9283ad7049adc1590e
Nikita Seleckis <nikita@seleckis.lv>
2015-08-04 19:07:11 +0300

    about line height

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -7,7 +7,7 @@
   <meta name="description" content="<?= ($page->isHomepage()) ? $site->description() : $page->meta_description() ?>"/>
   <meta name="author" content="<?= $site->author() ?>"/>
   <meta name="keywords" content="<?= $page->keywords() ?>"/><? snippet('favicons') ?>
-  <?= css('assets/css/c1705c0d.main.cache.css') ?>
+  <?= css('assets/css/ed08e3a9.main.cache.css') ?>
   <? snippet('facebook_aud') ?><!--[if (!IE)|(gt IE 8)]><!-->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script><!--<![endif]-->
   <!--[if lte IE 8]>
