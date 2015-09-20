c3758f3401a70bb4e6a8f0395130f7fd6b0fd833
Nikita Seleckis <nikita@seleckis.lv>
2015-09-20 15:44:15 +0300

    files recompiled

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -7,7 +7,7 @@
   <meta name="description" content="<?= ($page->isHomepage()) ? $site->description() : $page->meta_description() ?>"/>
   <meta name="author" content="<?= $site->author() ?>"/>
   <meta name="keywords" content="<?= $page->keywords() ?>"/><? snippet('favicons') ?>
-  <?= css('assets/css/6275f051.main.cache.css') ?>
+  <?= css('assets/css/74033af7.main.cache.css') ?>
   <? snippet('facebook_aud') ?><!--[if (!IE)|(gt IE 8)]><!-->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script><!--<![endif]-->
   <!--[if lte IE 8]>
