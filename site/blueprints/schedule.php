28366146e2f09ad0a371dba616358be49f30a2bf
Nikita Seleckis <nikita@seleckis.lv>
2015-10-27 16:48:23 +0200

    files recompiled

--- site/blueprints/schedule.php
+++ site/blueprints/schedule.php
@@ -7,12 +7,24 @@ fields:
 	title:
 		label: Title
 		type: text
+	day1_title:
+		label: Day 1 Title
+		type: text
 	day1:
 		label: Day 1
 		type: textarea
+	afterparty_title:
+		label: Afteparty Title
+		type: text
+	afterparty_desc:
+		label: Afteparty Desc
+		type: text
 	afterparty:
 		label: Afterparty
 		type: textarea
+	day2_title:
+		label: Day 2 Title
+		type: text
 	day2:
 		label: Day 2
 		type: textarea
--- site/snippets/schedule.php
+++ site/snippets/schedule.php
@@ -4,17 +4,42 @@
     <h1 class="section--lightgray__title"><?= $data->title() ?></h1>
   </div>
   <div class="section--lightgray__body">
-    <? $day1 = yaml($data->day1()) ?>
-    <? if(!empty($day1)): ?>
     <div class="wrapper">
+      <? $day1 = yaml($data->day1()) ?>
+      <? if(!empty($day1)): ?>
       <table class="t schedule">
+        <caption class="schedule__caption"><?= $data->day1_title() ?></caption>
         <tbody class="t-body"><? foreach($day1 as $line): ?>
           <tr class="t-body__tr">
             <td class="t-body__td--time"><?= $line["time"] ?></td>
             <td class="t-body__td"><? if($line["type"] == "speach"): ?><a href="#<?= $line['link'] ?>" class="js-scrollTo schedule__link"><span class="schedule__speach"><?= $line["title"] ?></span><span class="schedule__speaker"><?= $line["speaker"] ?></span></a><? else: ?><span class="schedule__action"><?= $line["title"] ?></span><? endif ?></td>
           </tr><? endforeach ?>
         </tbody>
-      </table>
-    </div><? endif ?>
+      </table><? endif ?>
+      <? $afterparty = yaml($data->afterparty()) ?>
+      <? if(!empty($afterparty)): ?>
+      <table class="t schedule">
+        <caption class="schedule__caption"><?= $data->afterparty_title() ?>
+          <div class="schedule__desc"><?= $data->afterparty_desc() ?></div>
+        </caption>
+        <tbody class="t-body"><? foreach($afterparty as $line): ?>
+          <tr class="t-body__tr">
+            <td class="t-body__td--time"><?= $line["time"] ?></td>
+            <td class="t-body__td"><? if($line["type"] == "speach"): ?><a href="#<?= $line['link'] ?>" class="js-scrollTo schedule__link"><span class="schedule__speach"><?= $line["title"] ?></span><span class="schedule__speaker"><?= $line["speaker"] ?></span></a><? else: ?><span class="schedule__action"><?= $line["title"] ?></span><? endif ?></td>
+          </tr><? endforeach ?>
+        </tbody>
+      </table><? endif ?>
+      <? $day2 = yaml($data->day2()) ?>
+      <? if(!empty($day2)): ?>
+      <table class="t schedule">
+        <caption class="schedule__caption"><?= $data->day2_title() ?></caption>
+        <tbody class="t-body"><? foreach($day2 as $line): ?>
+          <tr class="t-body__tr">
+            <td class="t-body__td--time"><?= $line["time"] ?></td>
+            <td class="t-body__td"><? if($line["type"] == "speach"): ?><a href="#<?= $line['link'] ?>" class="js-scrollTo schedule__link"><span class="schedule__speach"><?= $line["title"] ?></span><span class="schedule__speaker"><?= $line["speaker"] ?></span></a><? else: ?><span class="schedule__action"><?= $line["title"] ?></span><? endif ?></td>
+          </tr><? endforeach ?>
+        </tbody>
+      </table><? endif ?>
+    </div>
   </div>
 </section>
