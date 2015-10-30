618ba99cfe600196f873916e89bda57543233cee
Nikita Seleckis <nikita@seleckis.lv>
2015-10-30 12:25:54 +0200

    old sponsors templates renamed to sponsorship

---
+++ site/blueprints/sponsorship.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Sponsorship
+pages: sponsors_pack
+files: false
+fields:
+	title:
+		label: Title
+		type: text
+	foottitle:
+		label: Foot Title
+		type: text
