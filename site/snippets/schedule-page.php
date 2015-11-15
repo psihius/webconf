9a5ffd426e2ceb3ce1b0fd3605d53ee7cdf25196
Nikita Seleckis <nikita@seleckis.lv>
2015-11-15 09:10:56 +0200

    schedule updated to second day

--- site/snippets/schedule-page.php
+++ site/snippets/schedule-page.php
@@ -5,11 +5,11 @@
   </div>
   <div class="section--lightgray__body">
     <div class="wrapper">
-      <? $day1 = yaml($data->day1()) ?>
+      <? $day1 = yaml($data->day2()) ?>
       <? if(!empty($day1)): ?>
       <table class="t schedule">
-        <caption class="schedule__caption"><?= $data->day1_title() ?>
-          <div class="schedule__desc"><?= $data->day1_desc() ?></div>
+        <caption class="schedule__caption"><?= $data->day2_title() ?>
+          <div class="schedule__desc"><?= $data->day2_desc() ?></div>
         </caption>
         <tbody class="t-body">
           <? foreach($day1 as $key => $line): ?>
@@ -35,40 +35,6 @@
               <div class="schedule__time"><?= $line["time"] ?></div>
             </td>
             <td class="t-body__td"><? if($line["type"] == "speach"): ?><span class="schedule__speaker"><?= $line["speaker"] ?></span><span class="schedule__speach"><?= $line["title"] ?></span><span class="schedule__desc"><?= $line["desc"] ?></span><? else: ?><span class="schedule__action"><?= $line["title"] ?></span><? endif ?>
-            </td>
-          </tr><? endforeach ?>
-        </tbody>
-      </table><? endif ?>
-      <? $afterparty = yaml($data->afterparty()) ?>
-      <? if(!empty($afterparty)): ?>
-      <table class="t schedule">
-        <caption class="schedule__caption"><?= $data->afterparty_title() ?>
-          <div class="schedule__desc--accented"><?= kirbytext($data->afterparty_desc()) ?></div>
-        </caption>
-        <tbody class="t-body"><? foreach($afterparty as $line): ?>
-          <tr class="t-body__tr">
-            <td class="t-body__td--time">
-              <div class="schedule__time"><?= $line["time"] ?></div>
-            </td>
-            <td class="t-body__td">
-              <? if($line["type"] == "speach"): ?>
-              <?= print_r($line) ?><span class="schedule__speach"><?= $line["title"] ?></span><span class="schedule__desc"><?= $line["desc"] ?></span><span class="schedule__speaker"><?= $line["speaker"] ?></span><? else: ?><span class="schedule__action"><?= $line["title"] ?></span><? endif ?>
-            </td>
-          </tr><? endforeach ?>
-        </tbody>
-      </table><? endif ?>
-      <? $day2 = yaml($data->day2()) ?>
-      <? if(!empty($day2)): ?>
-      <table class="t schedule">
-        <caption class="schedule__caption"><?= $data->day2_title() ?>
-          <div class="schedule__desc"><?= $data->day2_desc() ?></div>
-        </caption>
-        <tbody class="t-body"><? foreach($day2 as $line): ?>
-          <tr class="t-body__tr">
-            <td class="t-body__td--time">
-              <div class="schedule__time"><?= $line["time"] ?></div>
-            </td>
-            <td class="t-body__td"><? if($line["type"] == "speach"): ?><span class="schedule__speach"><?= $line["title"] ?></span><span class="schedule__desc"><?= $line["desc"] ?></span><span class="schedule__speaker"><?= $line["speaker"] ?></span><? else: ?><span class="schedule__action"><?= $line["title"] ?></span><? endif ?>
             </td>
           </tr><? endforeach ?>
         </tbody>
